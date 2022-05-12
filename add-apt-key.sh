e4ffe3b3721401b4bb1e9a5524477daef0cf6415
Jp-Gigo <jpg-leboss@mail.fr>
2022-05-12 11:33:42 +0200

    Ajout commentaires utilisation

--- add-apt-key.sh
+++ add-apt-key.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-#Met en place les clé gpg dans /etc/apt/trusted.d/
-#Remplace apt-key-add deprecated
-
+#Met en place les clé gpg dans /usr/share/keyrings/
+#Remplace apt-key-add (deprecated)
+# https://stackoverflow.com/questions/68992799/warning-apt-key-is-deprecated-manage-keyring-files-in-trusted-gpg-d-instead
+#Il faut modifier le chemin dans le source.list concerné pour que apt
+#prenne en compte la clé gpg en question
+#ex:[arch=amd64 signed-by=/usr/share/keyrings/opera.gpg]
+#arch=amd64 n'est pas obligatoire.
 
 name=""
 url=""
@@ -17,5 +21,7 @@ if [ "${status}" -eq 0 ]; then
 	printf '%s : %s\n' "OK" "La clé GPG pour le projet '${name}' a bien été ajoutée."
 else
 	printf '%s : %s\n' "KO" "Il semble y avoir un soucis pour ajouter la clé GPG du projet '${name}' !"
-	exit 0
 fi
+
+## https://explainshell.com/explain?cmd=curl+-fsSL+example.org
+## https://explainshell.com/gpg
